302a8666f809981ff942a105252ac8d89ed8b90c
s3g <sgreenle@risd.edu>
2019-10-14 09:33:20 -0400

    example updates

---
+++ class6/teensy/teensyCC_prbd2/teensyCC_prbd2.ino
@@ -0,0 +1,162 @@
+/*
+
+  RISD CTC-2001
+  Programming Sound: Performance Systems
+  Prof. Shawn Greenlee
+
+  Teensy LC starter code - PWM out
+
+  !!! remember, only use the 3.3-volt power pin. DO NOT USE 5-volt or you may fry your board!!!
+
+  Code adapted from Yann Seznec www.yannseznec.com
+  and pjrc (makers of Teensy). These sources are in public domain.
+
+  You must have Teensyduino installed:
+  https://www.pjrc.com/teensy/td_download.html
+
+  When uploading code to board remember to set: Tools > Board > Teensy LC
+
+  &
+
+  Set: Tools > USB Type > MIDI
+
+*/
+
+// set analog input pins
+// set for none in this example
+
+int const numPins = 8; //  set number of analog inputs to use
+int currentVal[numPins];
+int newVal[numPins];
+int analogPins[] = {
+0, 1, 4, 5, 7, 10, 11, 12,  // set which analog pins to use
+};
+
+// set digital input pins
+// set for none in this exampl
+
+int const numDigPinsCC = 4; // number of digital pins to send CC
+int currentDigcc[numDigPinsCC];
+int digitalpincc[] = {
+  // which digital pins to use for sending CC
+2, 5, 7, 8
+};
+
+int digInputcc[numDigPinsCC];
+
+// the MIDI channel number to send messages
+const int channel = 1;
+
+elapsedMillis msec = 0;
+
+void HandleCC(byte channel, byte number, byte value) {
+
+  // Note: not all digital pins can be PWM, see your Teensy LC chart for which can be set.
+  // These should be D3, D4, D6, D9, D10, D16, D17, D20, D22, D23
+
+
+  if (number == 61) {
+    analogWrite(3, value * 2);
+  }
+
+  if (number == 62) {
+    analogWrite(4, value * 2);
+  }
+
+
+  if (number == 63) {
+    analogWrite(6, value * 2);
+  }
+
+
+  if (number == 64) {
+    analogWrite(9, value * 2);
+  }
+
+
+  if (number == 65) {
+    analogWrite(10, value * 2);
+  }
+
+
+  if (number == 66) {
+    analogWrite(16, value * 2);
+  }
+
+
+  if (number == 67) {
+    analogWrite(17, value * 2);
+  }
+
+  if (number == 68) {
+    analogWrite(20, value * 2);
+  }
+
+
+  if (number == 69) {
+    analogWrite(22, value * 2);
+  }
+
+  if (number == 70) {
+    analogWrite(23, value * 2);
+  }
+
+
+
+}
+
+void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, OUTPUT);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+  pinMode(20, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+
+  usbMIDI.setHandleControlChange(HandleCC);
+
+}
+
+void loop() {
+  if (msec >= 20) {
+    msec = 0;
+
+    // digital pins sending CC
+
+    for (int i = 0; i < numDigPinsCC; i++) {
+      if (digitalRead(digitalpincc[i]) == 1 && currentDigcc[i] == 0) {
+        usbMIDI.sendControlChange(i + 51, 0, channel);
+        currentDigcc[i] = 1;
+      }
+      if (digitalRead(digitalpincc[i]) == 0  && currentDigcc[i] == 1) {
+        usbMIDI.sendControlChange(i + 51, 127, channel);
+        currentDigcc[i] = 0;
+      }
+    }
+
+    // analog pins
+
+    for (int i = 0; i < numPins; i++) {
+
+      newVal[i] = analogRead(analogPins[i]);
+
+      if (abs(newVal[i] - currentVal[i]) > 3) {
+        usbMIDI.sendControlChange(i + 1, newVal[i] >> 3, channel);
+        currentVal[i] = newVal[i];
+      }
+    }
+  usbMIDI.sendControlChange(71, constrain(map(touchRead(0), 1000, 5000, 0, 127), 0, 127), 1);
+  usbMIDI.sendControlChange(72, constrain(map(touchRead(1), 1000, 5000, 0, 127), 0, 127), 1);
+  }
+
+  usbMIDI.read();
+
+}
